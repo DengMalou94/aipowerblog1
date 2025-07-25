4d288e760c275947e59168209ccb181f1d2abcd3
DengMalou94 <dengmalou74@gmail.com>
2025-07-25 11:55:41 +0300

    Delete pages folder

--- my-app/pages/index.js
+++
@@ -1,9 +0,0 @@
-// pages/index.js
-export default function Home() {
-  return (
-    <main>
-      <h1>Welcome to AI Powered Blog</h1>
-      <p>This is your homepage served by Next.js.</p>
-    </main>
-  );
-}
